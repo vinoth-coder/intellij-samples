525a302e1965265fed028f343f3f4a63dc7b0a6f
vinoth-pt6023 <vinoth.kumarr@zohocorp.com>
2023-05-15 16:36:11 +0530

    addNumber

--- java-samples/src/main/java/com/jetbrains/running/HelloWorld.java
+++ java-samples/src/main/java/com/jetbrains/running/HelloWorld.java
@@ -1,6 +1,7 @@
 public HelloWorld{
     public static void main(String args[]){
-        int i=0;
+        int num1=2,num2=2;
+        System.out.println("The addition of two numbers is "+(num1+num2));
         String name="Vinoth";
         System.out.println("Hello World");
         System.out.println("The User name is "+name);
