99398ca870e72d0be7803bb8b519fa7a62afeea3
vinoth-pt6023 <vinoth.kumarr@zohocorp.com>
2023-05-15 13:49:44 +0530

    nameInclusion

--- java-samples/src/main/java/com/jetbrains/running/HelloWorld.java
+++ java-samples/src/main/java/com/jetbrains/running/HelloWorld.java
@@ -1,5 +1,8 @@
 public HelloWorld{
     public static void main(String args[]){
+        int i=0;
+        String name="Vinoth";
         System.out.println("Hello World");
+        System.out.println("The User name is "+name);
     }
 }
